66b1d2076e24bc70c85ae02931b88c83d16ca873
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-11-24 14:55:00 +0100

    changed name of django app from session to session_info

--- meet/urls.py
+++ meet/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('session/', include("session.urls")),
+    path('session_info/', include("session_info.urls")),
 ]
