14bffa8413fbf500f80831bf71acbe77d42e6738
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-11-24 14:49:07 +0100

    add session django app

---
+++ session/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import reverse
+
+def view(request):
+    session = {
+        "_session_key_id": request.session._session_key
+    }
+    for key, val in request.session.items():
+        session[key] = val
+    return JsonResponse(session)
+
+def do_set(request, key, val):
+    request.session[key] = val
+    return HttpResponseRedirect(reverse('session_view'))
+
+def do_del(request, key):
+    del request.session[key]
+    return HttpResponseRedirect(reverse('session_view'))
